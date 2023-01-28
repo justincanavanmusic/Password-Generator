8abaad0c0a00391bf0447074d0ad17840992b7ee
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2023-01-27 22:31:18 -0600

    final final edits.. added READme

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,30 @@
 // variables
+
+//created variables for each character set
 let upLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 let lowLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-let specials = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"]
+let specials = [' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '>', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
 
-// let allChars = upLetters.concat(lowLetters, numbers, specials);
 
 // querySelectors
 let generateBtn = document.querySelector("#generate");
 
 // Logic
             //PASSWORD OPTIONS 
+
+                    //parseInt will check for data that isn't a number (NaN)      
 function passwordOptions() {
     let passwordLength = parseInt(prompt("How many characters would you like your password to contain? 8-128"));
+    if(isNaN(passwordLength)) {     //if password length is not a number, send alert to user
+      alert("You have to choose a number.");
+        return null  //return null stops the code from continuing   
+    }
 
-    if (passwordLength < 8 || passwordLength > 128) {
+    if (passwordLength < 8 || passwordLength > 128) {   //if pass length is less than 8 or more than 128, send user an alert
         alert("Password length must be greater than 7 and less than 129");
         return null;
     }
@@ -25,46 +32,43 @@ function passwordOptions() {
     //     alert("You must select a number from 8-128");
     // }
 
-    let useLowers = confirm('Click OK to confirm using lowercase letters.');
+    let useLowers = confirm('Click OK to confirm using lowercase letters.');  //allows user to select if they want lowercase
     console.log(useLowers);
 
-    let useUppers = confirm('Click OK to confirm using uppercase letters.');
+    let useUppers = confirm('Click OK to confirm using uppercase letters.');  //same as above for upper
     console.log(useUppers);
 
-    let useNumbers = confirm('Click OK to confirm using numbers.');
+    let useNumbers = confirm('Click OK to confirm using numbers.'); //same as above for numbers
     console.log(useNumbers);
 
-    let useSpecials = confirm('Click OK to confirm using special characters.');
+    let useSpecials = confirm('Click OK to confirm using special characters.');   //same as above specials
     console.log(useSpecials);
 
     if(useNumbers === false && useLowers===false && useUppers===false && useSpecials===false) 
-{ alert('Must select at least one character type!');
+{ alert('Must select at least one character type!');  //user must select a type or get an alert
 }
-
+    //object that stores all relevant info for user password
      let userPassword = {
      length: passwordLength,
      hasLower: useLowers,
      hasNumber: useNumbers,
      hasUpper: useUppers,
      hasSpecial: useSpecials
-  }
+  }      //return saves all the relevant info into the object
   return userPassword;
-}
-//test run below
-// passwordOptions();
-
+}     
 
 
                             //GENERATE PASSWORD
 
 function generatePassword() {
-    let userOptions = passwordOptions();
-    let possibleChars= [];
-    let guarCharacters = [];  //guaranteed
-    let remainingCharacters = [];
+    let userOptions = passwordOptions();   //storing password options function into user options variable
+    let possibleChars= [];    //array of possible characters for the password to choose from
+    let guarCharacters = [];  //array of guaranteed characters to make sure each variable is shown at least once... dependant on user input
+    let remainingCharacters = []; //since the guaranteed characters are pushed (added onto array), code was developed to add the remaining characters (ie if we selected yes to all 4 "confirms" and we chose 12 as our number, this logic would produce the remaining 8 characters needed)
                           
-    if(userOptions.hasNumber===true) {
-      possibleChars=possibleChars.concat(numbers);
+    if(userOptions.hasNumber===true) {        //if user chooses to have numbers
+      possibleChars=possibleChars.concat(numbers);    //.concat adds numbers variable to the possible character pool; same for 3 below
       guarCharacters.push(numbers[Math.floor(Math.random() * numbers.length)]);
     }
     if(userOptions.hasUpper===true) {
@@ -82,36 +86,35 @@ function generatePassword() {
     // console.log(guaranteedChars);
     // console.log(possibleChars);
 
-    let guarLength=guarCharacters.length;
+    let guarLength=guarCharacters.length;   //stores guar char length in guarLength
                                     //user options minus password length
-        for (let index = 0; index < (userOptions.length-guarLength); index++) { 
+        for (let index = 0; index < (userOptions.length-guarLength); index++) {   //for loop runs until we have the users number selection(ie if user selects 12 the for loop will run 12 times)     user options-guarLength subtracts the amount of guaranteed characters from the array. since they were pushed and added to the total of the array, they need to be subtracted to get the desired number.
           remainingCharacters.push(possibleChars[Math.floor(Math.random() * possibleChars.length)]);
-        }
-        let almostPass = guarCharacters.concat(remainingCharacters);
-        let finalFinalPass=almostPass.join("");
+        }                         
+        let guarPlusRemain = guarCharacters.concat(remainingCharacters);  //concatenates guaranteed characters and remaining characters and stores in guarPlusRemain variable
+        let finalPass=guarPlusRemain.join(""); //.join joins the separate strings into one string, making the password
+        
         
         // console.log(finalFinalPass);
 
-        return finalFinalPass;
+        return finalPass;
 
   }
 
-
-// generatePassword();
 
 
                         //WRITE PASSWORD
 
 function writePassword() {
 
-    let password = generatePassword();
-    let passwordText = document.querySelector("#password");
-    passwordText.innerHTML = password;
-    //why didn't .value work?
+    let password = generatePassword();  //stores generate password function in password variable
+    let passwordText = document.querySelector("#password");   //query selector goes into HTML doc and selects #password
+    passwordText.value = password;
+    
 
 }
 
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); //when you click the button, write password function is called
 
 
 
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Password-Generator
+
+## Descripton 
+This repo contains the code to run a random password generator meeting a specific set of criteria
+
+## Usage
+Live link below:
+
+
+
+### Resources
+
+-bootcamp materials/instruction
+-pseudocode from TA Sam in class
+-worked with study groups, Dom, Huda, and more
+-worked together with friend in coding Johnny Contreras
